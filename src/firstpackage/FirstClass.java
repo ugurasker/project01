7a436ede79f9373baf92f7917fe2262d3d193da7
Ugur Askar <ugurasker@gmail.com>
2021-04-24 13:38:28 +0200

    second commit from local master

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,8 @@ package firstpackage;
 public class FirstClass {
 	
 	public static void main (String[] args) {
-		
+		System.out.println("First commit from local master");
+		System.out.println("Second line of code I am typing");
 	}
 	
 
